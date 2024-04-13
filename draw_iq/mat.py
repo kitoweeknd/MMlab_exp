4ff0fe18d1a17feb25bc9b985550fe68b83da656
Frank <108741470+kitoweeknd@users.noreply.github.com>
2024-04-13 10:47:03 +0800

    完善画图函数

--- draw_iq/mat.py
+++ draw_iq/mat.py
@@ -27,12 +27,12 @@ TODO
 '''
 
 #
-time_duration = 0.03  # 预设时间窗的长度(关键参数)，决定x尺度
+time_duration = 0.1  # 预设时间窗的长度(关键参数)，决定x尺度
 fs = 100e6  # 采样带宽要和采样设备匹配,实验室设备的采样带宽一般为15MHZ，原论文中的最大采样带宽为100MHZ，采样带宽决定y轴尺度
 slice_point = int(fs*time_duration)
 
 fly_name = 'AVATA'  # 预设无人机的名称
-sourceFolderPath = 'E:/360MoveData/Users/sam826001/Desktop/AVATA'  # 预设读取的目录
+sourceFolderPath = 'E:/360MoveData/Users/sam826001/Desktop/AVATA/'  # 预设读取的目录
 targetFolderPath = 'E:/360MoveData/Users/sam826001/Desktop/temp/'  # 预设保存的目录
 
 stft_point = 2048  # 采样点数是每次y轴用的点数,1024就够用了
@@ -40,6 +40,7 @@ stft_point = 2048  # 采样点数是每次y轴用的点数,1024就够用了
 
 def main():
     # 设置一个循环使程序可以连续的读取一个文件中的所有图片
+    i = 0
     while True:
         re_files = os.listdir(sourceFolderPath)
         for file in re_files:
@@ -50,18 +51,19 @@ def main():
             # data_Q1 = data['RF0_Q'][0]  # 同一组信号的IQ读一路就行
             data_I2 = data['RF1_I'][0]
             # data_Q2 = data['RF1_I'][0]  # 同一组信号的IQ读一路就行
-            i = 0
-            while (i+1)*slice_point <= len(data_I1):
+            j = 0
+            while (j+1)*slice_point <= len(data_I1):
                 # 先画第一个通道的
                 f_I1, t_I1, Zxx_I1 = stft(data_I1[i*slice_point: (i+1) * slice_point],
                                  fs, window=windows.hamming(stft_point), nperseg=stft_point)
                 augmentation_Zxx1 = 20*np.log10(np.abs(Zxx_I1))  # 是否选择增强
                 # 画第一组IQ
-                plt.figure()
-                plt.pcolormesh([i*t_I1, (i+1)*t_I1], f_I1, augmentation_Zxx1)  # 画图时在颜色标尺上应用了一个软件增益使信号在图中更加明显，其实不用也行，用了还会丢失距离信息
+                # plt.figure()
+                plt.ioff()  # 关闭可视窗
+                plt.pcolormesh(t_I1, f_I1, augmentation_Zxx1)  # 画图时在颜色标尺上应用了一个软件增益使信号在图中更加明显，其实不用也行，用了还会丢失距离信息
                 # plt.colorbar()  # 在做实验观察信号的时候可以加一个颜色标尺，做数据集时不用加颜色标尺
                 plt.title(fly_name + (str(i)))
-                plt.savefig(targetFolderPath + fly_name + str(i) + '2.4GHZ.jpg')
+                plt.savefig(targetFolderPath + fly_name + str(i) + '2.4GHZ.jpg', dpi=300)  # dpi设置了图片保存的清晰度，300对应1K的图片
                 plt.close()
 
                 # 再画第二个通道的
@@ -69,14 +71,19 @@ def main():
                                  fs, window=windows.hamming(stft_point), nperseg=stft_point)
                 augmentation_Zxx2 = 20*np.log10(np.abs(Zxx_I2))  # 是否选择增强
                 # 画第二组IQ
-                plt.figure()
+                # plt.figure()
                 plt.pcolormesh(t_I2, f_I2, augmentation_Zxx2)  # 画图时在颜色标尺上应用了一个软件增益使信号在图中更加明显，其实不用也行，用了还会丢失距离信息
-                plt.colorbar()  # 在做实验观察信号的时候可以加一个颜色标尺，做数据集时不用加颜色标尺
+                # plt.colorbar()  # 在做实验观察信号的时候可以加一个颜色标尺，做数据集时不用加颜色标尺
                 plt.title(fly_name + (str(i)))
-                plt.savefig(targetFolderPath + fly_name + str(i) + '5.8GHZ.jpg')
+                plt.savefig(targetFolderPath + fly_name + str(i) + '5.8GHZ.jpg', dpi=300)
                 plt.close()
 
                 i += 1
+                j += 1
+
+            print(file + 'Done')
+        print('All Done')
+        return 0
 
 
 if __name__ == '__main__':
