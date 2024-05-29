d5a63d75cdb2226ae2b96a83fe1b3809f9279d55
Frank <108741470+kitoweeknd@users.noreply.github.com>
2024-05-29 23:17:47 +0800

    添加了CO-DINO的函数，还没完全改好

---
+++ Drone_Thesis/res_co_dino/co_dino_5scale_swin_l_16xb1_1x_coco_signals.py
@@ -0,0 +1,53 @@
+# 还得研究一下
+
+_base_ = 'D:\ML_Project\mmDLtoolbox\projects\CO-DETR\configs\codino\co_dino_5scale_swin_l_16xb1_1x_coco.py'
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=9))
+)
+
+# 修改数据集相关配置
+data_root = 'E:/数据集历史数据/MMlab/signal_coco/'
+metainfo = {
+    'classes': ('DJ_FlightCon',
+                'DJ_PCTrans',
+                'singal1',
+                'singal2',
+                'singal3',
+                'singal4',
+                'singal5',
+                'singal6',
+                'singal7',
+                ),
+    'palette': [
+        (220, 20, 60),
+    ]
+}
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='train/annotation_coco.json',
+        data_prefix=dict(img='train/')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='val/annotation_coco.json',
+        data_prefix=dict(img='val/')))
+
+# 修改评价指标相关配置
+val_evaluator = dict(ann_file=data_root + 'val/annotation_coco.json')
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file=data_root + 'test/annotation_coco.json',
+        data_prefix=dict(img='test/')))
+test_evaluator = dict(
+    ann_file=data_root + 'test/annotation_coco.json',
+    outfile_prefix='./work_dirs/signals_test/test')
+
+load_form = ''
