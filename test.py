6de2aef2ab3abcaf52a63a8ca1477666a8ba86e0
server <2403329962@qq.com>
2024-10-22 21:26:01 +0800

    开始revision，把要补的实验的训练函数写好了

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import torch
-in_channels = [2, 3, 5, 7]
-scales = [340, 170, 84, 43]
-inputs = [torch.rand(1, c, s, s)
-               for c, s in zip(in_channels, scales)]
-print(inputs[0].shape)
-print(inputs[1].shape)
-print(inputs[2].shape)
-print(inputs[3].shape)
+import matplotlib.pyplot as plt
+import numpy as np
 
-# self = ChannelMapper(in_channels, 11, 3).eval()
-# outputs = self.forward(inputs)
-print(inputs)
+# 定义数据矩阵
+C = np.array([[0, 2, 4, 6],
+              [8, 10, 12, 14],
+              [16, 18, 20, 22]])
+
+# 显示图像
+plt.imshow(C)
+
+# 添加颜色条
+plt.colorbar()
+
+plt.show()
+ 
